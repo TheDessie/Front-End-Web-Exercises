79eaff6d397a4ea0675ac4dd1b0cc757a316ccdd
TheDessie <dessie.d.dimitrova@gmail.com>
2020-11-22 09:32:06 +0200

    Implement Exercise 2

---
+++ Week4/Ex2.js
@@ -0,0 +1,18 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+        this.planet = "Земя";
+    }
+    printPerson() {
+        console.log(`Здравей ${this.name} от планетата ${this.planet}`)
+    }
+}
+
+let p1 = new Person("Чък Норис");
+p1.printPerson();
+
+let p2 = new Person("Пешо Пешов");
+p2.printPerson();
+
+let p3 = new Person("Гошо Гошов");
+p3.printPerson();
